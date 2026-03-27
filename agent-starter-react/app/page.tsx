758d0dced87bd412ce12d0f493382cd680a23a11
Manas Khadke <manaskhadke04@gmail.com>
2026-03-27 13:53:23 +0530

    Redirect home page to Jarvis gesture HUD

--- agent-starter-react/app/page.tsx
+++ agent-starter-react/app/page.tsx
@@ -1,10 +1,5 @@
-import { headers } from 'next/headers';
-import { App } from '@/components/app';
-import { getAppConfig } from '@/lib/utils';
+import { redirect } from 'next/navigation';
 
-export default async function Page() {
-  const hdrs = await headers();
-  const appConfig = await getAppConfig(hdrs);
-
-  return <App appConfig={appConfig} />;
+export default function Page() {
+  redirect('/gesture.html');
 }
